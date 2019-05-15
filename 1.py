7722766efc380345278a4db7e299bc59011715fa
abhishek007yadav <you@example.com>
2019-05-14 23:52:38 -0700

    first push

---
+++ 1.py
@@ -0,0 +1,22 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=5
+>>> y=3
+>>> print(x+Y)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    print(x+Y)
+NameError: name 'Y' is not defined
+>>> x=5
+>>> y=3
+>>> print(x+y)
+8
+>>> x=2
+>>> y=3
+>>> print(x**y)
+8
+>>> x=8
+>>> y=3
+>>> print(x%y)
+2
+>>> 
